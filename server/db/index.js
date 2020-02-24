705aae31c8574287122b603fa1e4221e8bc42be4
remi-gai <56744348+remi-gai@users.noreply.github.com>
2020-02-24 12:20:39 -0800

    refactored db.js to connect to EC2 Cassandra DB

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,17 @@
 const cassandra = require('cassandra-driver');
 
+//Local Cassandra DB
+// const client = new cassandra.Client({
+//   contactPoints: ['127.0.0.1'],
+//   localDataCenter: 'datacenter1',
+//   keyspace: 'sdc_airbusybeavers'
+// });
+
+//EC2 Cassandra DB
 const client = new cassandra.Client({
-  contactPoints: ['127.0.0.1'],
+  contactPoints: ['18.144.166.104'],
   localDataCenter: 'datacenter1',
-  keyspace: 'sdc_airbusybeavers'
+  keyspace: 'sdc'
 });
 
 module.exports = client;
