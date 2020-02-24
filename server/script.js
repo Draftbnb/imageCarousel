0137faab23ffeb2bfaa768fbd75f8155de1cda80
remi-gai <56744348+remi-gai@users.noreply.github.com>
2020-02-24 15:50:23 -0800

    increased k6 test from 200ms to 2000ms

--- server/script.js
+++ server/script.js
@@ -35,7 +35,7 @@ export default function() {
   let res = http.get(`http://localhost:3000/`);
   check(res, {
     "status was 200": (r) => r.status == 200,
-    "transaction time OK": (r) => r.timings.duration < 200
+    "transaction time OK": (r) => r.timings.duration < 2000
   });
   sleep(0.001);
 };
